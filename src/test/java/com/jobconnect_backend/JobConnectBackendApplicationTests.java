fc436b529ae59f0759b5cc9a9ecce80bfa608254
Dao Thanh Phu <21097761.phu@student.iuh.edu.vn>
2025-10-05 22:28:23 +0700

    create project jobconnect

---
+++ src/test/java/com/jobconnect_backend/JobConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobconnect_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobConnectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
