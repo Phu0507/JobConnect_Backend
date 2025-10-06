0dc09017a9c57bd0da8f2e156fbba8c24fdae283
Dao Thanh Phu <21097761.phu@student.iuh.edu.vn>
2025-10-06 15:29:23 +0700

    config default value

---
+++ src/main/java/com/jobconnect_backend/defaults/DefaultValue.java
@@ -0,0 +1,9 @@
+package com.jobconnect_backend.defaults;
+
+public final class DefaultValue {
+    public static final String AVATAR_URL_DEFAULT = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+    public static final String WS_TOPIC_CONVERSATION = "/topic/conversation/";
+    public static final String WS_TOPIC_COUNT_UNREAD = "/topic/unread-count/";
+    public static final String WS_TOPIC_DATA_CONVERSATION = "/topic/conversation-data/";
+    public static final String WS_TOPIC_NOTIFICATION = "/topic/notifications/";
+}
