af90adee4f1004379097df746ff5830a86e22f11
Dao Thanh Phu <21097761.phu@student.iuh.edu.vn>
2025-10-07 22:05:20 +0700

    add delete update job category

--- src/main/java/com/jobconnect_backend/controllers/JobCategoryController.java
+++ src/main/java/com/jobconnect_backend/controllers/JobCategoryController.java
@@ -1,12 +1,11 @@
 package com.jobconnect_backend.controllers;
 
 import com.jobconnect_backend.dto.response.JobCategoryResponse;
+import com.jobconnect_backend.dto.response.SuccessResponse;
 import com.jobconnect_backend.services.IJobCategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,26 @@ public class JobCategoryController {
     @GetMapping("/all")
     public ResponseEntity<List<JobCategoryResponse>> getAllJobCategories() {
         return ResponseEntity.ok(jobCategoryServiceImpl.getAllJobCategories());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<SuccessResponse> addJobCategory(String jobCategoryName) {
+        jobCategoryServiceImpl.addJobCategory(jobCategoryName);
+        return ResponseEntity.ok(new SuccessResponse("Job Category added successfully"));
+    }
+
+    @DeleteMapping("/delete/{jobCategoryId}")
+    public ResponseEntity<SuccessResponse> deleteJobCategory(@PathVariable Integer jobCategoryId) {
+        jobCategoryServiceImpl.deleteJobCategory(jobCategoryId);
+        return ResponseEntity.ok(new SuccessResponse("JobCategory deleted successfully"));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<SuccessResponse> updateJobCategory(
+            @RequestParam Integer jobCategoryId,
+            @RequestParam String jobCategoryName
+    ) {
+        jobCategoryServiceImpl.updateJobCategory(jobCategoryId, jobCategoryName);
+        return ResponseEntity.ok(new SuccessResponse("JobCategory updated successfully"));
     }
 }
--- src/main/java/com/jobconnect_backend/services/impl/JobCategoryServiceImpl.java
+++ src/main/java/com/jobconnect_backend/services/impl/JobCategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.jobconnect_backend.services.impl;
 
 import com.jobconnect_backend.dto.response.JobCategoryResponse;
 import com.jobconnect_backend.entities.JobCategory;
+import com.jobconnect_backend.exception.BadRequestException;
 import com.jobconnect_backend.repositories.JobCategoryRepository;
 import com.jobconnect_backend.services.IJobCategoryService;
 import lombok.RequiredArgsConstructor;
@@ -27,16 +28,25 @@ public class JobCategoryServiceImpl implements IJobCategoryService {
 
     @Override
     public void addJobCategory(String jobCategoryName) {
-
+        if(jobCategoryRepository.existsByName(jobCategoryName)){
+            throw new BadRequestException("Job Category already exists");
+        }
+        JobCategory jobCategory = JobCategory.builder()
+                .name(jobCategoryName)
+                .build();
+        jobCategoryRepository.save(jobCategory);
     }
 
     @Override
     public void deleteJobCategory(Integer jobCategoryId) {
-
+        jobCategoryRepository.deleteById(jobCategoryId);
     }
 
     @Override
     public void updateJobCategory(Integer jobCategoryId, String jobCategoryName) {
-
+        JobCategory jobCategory = jobCategoryRepository.findById(jobCategoryId).orElseThrow(
+                () -> new BadRequestException("Job Category not found"));
+        jobCategory.setName(jobCategoryName);
+        jobCategoryRepository.save(jobCategory);
     }
 }
